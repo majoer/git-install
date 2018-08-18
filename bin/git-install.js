b46630a3c048732b1e7842558da74201d9360d4e
Mats Jørgensen <matsjoer@gmail.com>
2018-08-18 18:08:19 +0200

    Initial commit

---
+++ bin/git-install.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+const shell = require('shelljs');
+const ArgumentParser = require('argparse').ArgumentParser;
+
+const parser = new ArgumentParser();
+
+parser.addArgument(['projectName']);
+parser.addArgument(['-t', '--template']);
+
+const args = parser.parseArgs();
+
+console.log(args);
+
+if (shell.exec(`git clone ${args.template} ${args.projectName}`).code !== 0) {
+  exit();
+}
+
+shell.cd(args.projectName);
+shell.rm('-rf', '.git');
+shell.exec('git init');
+// shell.exec('git add ./*');
+shell.exec('git add .');
+shell.exec('git commit -m "Initial Commit"');
+
+function exit(msg) {
+  if (msg) {
+    shell.echo(msg);
+  }
+
+  shell.exit('1');
+}
